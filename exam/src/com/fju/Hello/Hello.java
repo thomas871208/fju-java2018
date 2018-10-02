3d1caabb976ecd39e9c1e7895ce9640be12ac809
user <user@es302n11>
2018-10-02 11:06:19 +0800

    Hello

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -5,6 +5,11 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int schoolYear;
+		System.out.println("weight");
+		System.out.println("name");
+		float weight = 64.5f
+				
 	}
 
 }
